4214c1f4d98c37ee942b63cc3a0caa7e33ab47f4
MDiaz8778 <maurodiaz8778@gmail.com>
2024-10-17 13:51:06 -0300

    asd

--- src/components/cards.js
+++ src/components/cards.js
@@ -3,16 +3,13 @@ import { useState } from 'react';
 import '../styles/cards.css';
 
 const Card = ({ title, description, imgSrc, onSale, price, originalPrice }) => {
-  // Estado para el contador de cantidad
   const [quantity, setQuantity] = useState(1);
 
-  // Funciones para aumentar y disminuir la cantidad
   const increaseQuantity = () => setQuantity(quantity + 1);
   const decreaseQuantity = () => {
-    if (quantity > 1) setQuantity(quantity - 1); // Asegura que no sea menor que 1
+    if (quantity > 1) setQuantity(quantity - 1); 
   };
 
-  // Función para truncar la descripción
   const truncateDescription = (desc, maxLength) => {
     if (desc.length > maxLength) {
       return desc.substring(0, maxLength) + "...";
@@ -22,7 +19,6 @@ const Card = ({ title, description, imgSrc, onSale, price, originalPrice }) => {
 
   return (
     <div className="card">
-      {/* Mini card "On Sale" en la esquina superior izquierda */}
       {onSale && (
         <div className="mini-card on-sale">
           <span className="green-dot"></span>
@@ -30,13 +26,11 @@ const Card = ({ title, description, imgSrc, onSale, price, originalPrice }) => {
         </div>
       )}
 
-      {/* Mini card "In Stock" en la esquina superior izquierda */}
       <div className="mini-card">
         <span>In Stock</span>
         <FaClock className="icon" />
       </div>
 
-      {/* Contador en la esquina superior derecha */}
       <div className="counter">
         <span className="quantity">{quantity}</span>
         <div className="counter-controls">
@@ -45,15 +39,14 @@ const Card = ({ title, description, imgSrc, onSale, price, originalPrice }) => {
         </div>
       </div>
 
-      {/* Contenido de la tarjeta */}
       <div className="img-cards">
-        <img src={imgSrc} alt={title} /> {/* Asegúrate de que imgSrc esté definido */}
+        <img src={imgSrc} alt={title} />
       </div>
       <div className="card-info">
         <h1 className="title">{title}</h1>
         <h2 className="price-container">
-          <span className="price">{price}</span> {/* Precio actual a la izquierda */}
-          {onSale && <span className="original-price">{originalPrice}</span>} {/* Precio original tachado a la derecha */}
+          <span className="price">{price}</span>
+          {onSale && <span className="original-price">{originalPrice}</span>}
         </h2>
 
 
